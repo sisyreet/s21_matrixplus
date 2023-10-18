7ad31c80d6b2c1a1e47a4f7c84bfbe8fc0884d6e
Signe Syreeta <sisyreet@oa-c1.msk.21-school.ru>
2023-10-18 21:44:00 +0300

    added a test

--- tests.cpp
+++ tests.cpp
@@ -1,18 +1,28 @@
 #include <gtest/gtest.h>
 #include "s21_matrix_oop.h"
-#include <utility> // ?
+// #include <utility> // ?
 
+TEST(ab, bc) {
+	S21Matrix m;
+	EXPECT_EQ(m.GetRows(), 1);
+	EXPECT_EQ(m.GetCols(), 1);
+	EXPECT_EQ(m(0,0), 0.0);
+}
+
+TEST(MatrixConstructorDestructor, ConvertingConstructorBadRows) {
+	EXPECT_THROW(S21Matrix(0, 0), std::invalid_argument);
+}
 
 TEST(MatrixConstructorDestructor, ConvertingConstructor) {
   int rows = 100;
-  int cols = 87;
+  int cols = 100;
   S21Matrix m(rows, cols);
 
   EXPECT_EQ(m.GetRows(), rows);
   EXPECT_EQ(m.GetCols(), cols);
 
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < cols; ++j) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
       EXPECT_EQ(m(i, j), 0.0);
     }
   }
