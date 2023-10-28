f66117775a5a71a1ec2402d99f84a70dcd2aba31
Signe Syreeta <sisyreet@at-j4.msk.21-school.ru>
2023-10-28 18:15:27 +0300

    foramt to google style

--- s21_matrix_oop.h
+++ s21_matrix_oop.h
@@ -1,51 +1,51 @@
 #ifndef S21_MATRIX_OOP_H
 #define S21_MATRIX_OOP_H
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 class S21Matrix {
-    public:
-		static const double epsilon;
-		
-        S21Matrix();
-        S21Matrix(int rows, int cols);
-        S21Matrix(const S21Matrix& other);
-        S21Matrix(S21Matrix&& other);
-        ~S21Matrix();
+ public:
+  static const double epsilon;
 
-        // getters and setters
-        int		GetRows();
-        int		GetCols();
-        void	SetRows(int rows);
-        void	SetCols(int cols);
+  S21Matrix();
+  S21Matrix(int rows, int cols);
+  S21Matrix(const S21Matrix &other);
+  S21Matrix(S21Matrix &&other);
+  ~S21Matrix();
 
-        // overloads
-		S21Matrix	operator+(S21Matrix &other) const;
-		S21Matrix	operator-(S21Matrix &other) const;
-		S21Matrix	operator*(S21Matrix &other) const;
-		bool		operator==(const S21Matrix &other); 
-		S21Matrix	&operator=(const S21Matrix &other);
-		S21Matrix	&operator+=(S21Matrix &other);
-		S21Matrix	&operator-=(S21Matrix &other);
-        double 		&operator()(int i, int j) const;
-		S21Matrix	&operator=(S21Matrix &&other);
+  // getters and setters
+  int GetRows();
+  int GetCols();
+  void SetRows(int rows);
+  void SetCols(int cols);
 
-		// member functions
-		bool		EqMatrix(const S21Matrix& other);
-		void		SumMatrix(const S21Matrix& other);
-		void		SubMatrix(const S21Matrix& other);
-		void		MulNumber(const double num);
-		void		MulMatrix(const S21Matrix& other);
-		S21Matrix	Transpose();
-		S21Matrix 	CalcComplements();
-		double 		Determinant();
-		S21Matrix	InverseMatrix();
-		S21Matrix	Minor(int rows, int cols) const;
-		
+  // overloads
+  S21Matrix operator+(S21Matrix &other) const;
+  S21Matrix operator-(S21Matrix &other) const;
+  S21Matrix operator*(S21Matrix &other) const;
+  S21Matrix operator*(double num) const;
+  bool operator==(const S21Matrix &other);
+  S21Matrix &operator=(const S21Matrix &other);
+  S21Matrix &operator+=(S21Matrix &other);
+  S21Matrix &operator-=(S21Matrix &other);
+  double &operator()(int i, int j) const;
+  S21Matrix &operator=(S21Matrix &&other);
 
-	private:
-        int 	rows_, cols_;
-        double	**matrix_;
+  // member functions
+  bool EqMatrix(const S21Matrix &other);
+  void SumMatrix(const S21Matrix &other);
+  void SubMatrix(const S21Matrix &other);
+  void MulNumber(const double num);
+  void MulMatrix(const S21Matrix &other);
+  S21Matrix Transpose();
+  S21Matrix CalcComplements();
+  double Determinant();
+  S21Matrix InverseMatrix();
+  S21Matrix Minor(int rows, int cols) const;
+
+ private:
+  int rows_, cols_;
+  double **matrix_;
 };
 
 #endif
